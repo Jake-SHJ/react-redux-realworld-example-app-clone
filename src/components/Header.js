fe793e6303724557d3c0c1b35402a7cad7fb98aa
jake <guswns1853@gmail.com>
2020-10-04 11:08:08 +0900

    fix: Header Component class to function converting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const LoggedOutView = props => {
+const LoggedOutView = (props) => {
   if (!props.currentUser) {
     return (
       <ul className="nav navbar-nav pull-xs-right">
-
         <li className="nav-item">
           <Link to="/" className="nav-link">
             Home
@@ -23,18 +22,16 @@ const LoggedOutView = props => {
             Sign up
           </Link>
         </li>
-
       </ul>
     );
   }
   return null;
 };
 
-const LoggedInView = props => {
+const LoggedInView = (props) => {
   if (props.currentUser) {
     return (
       <ul className="nav navbar-nav pull-xs-right">
-
         <li className="nav-item">
           <Link to="/" className="nav-link">
             Home
@@ -54,14 +51,15 @@ const LoggedInView = props => {
         </li>
 
         <li className="nav-item">
-          <Link
-            to={`/@${props.currentUser.username}`}
-            className="nav-link">
-            <img src={props.currentUser.image} className="user-pic" alt={props.currentUser.username} />
+          <Link to={`/@${props.currentUser.username}`} className="nav-link">
+            <img
+              src={props.currentUser.image}
+              className="user-pic"
+              alt={props.currentUser.username}
+            />
             {props.currentUser.username}
           </Link>
         </li>
-
       </ul>
     );
   }
@@ -69,23 +67,21 @@ const LoggedInView = props => {
   return null;
 };
 
-class Header extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light">
-        <div className="container">
+const Header = (props) => {
+  const { currentUser, appName } = props;
+  return (
+    <nav className="navbar navbar-light">
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          {appName.toLowerCase()}
+        </Link>
 
-          <Link to="/" className="navbar-brand">
-            {this.props.appName.toLowerCase()}
-          </Link>
+        <LoggedOutView currentUser={currentUser} />
 
-          <LoggedOutView currentUser={this.props.currentUser} />
-
-          <LoggedInView currentUser={this.props.currentUser} />
-        </div>
-      </nav>
-    );
-  }
-}
+        <LoggedInView currentUser={currentUser} />
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
